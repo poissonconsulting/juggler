1695f2fb743961d40a0d30823ea0f5ce7dc97c55
Joe Thorley <joe@poissonconsulting.ca>
2014-04-22 08:46:27 -0700

    pass_brackets now preserves names but not \n - fix

--- R/brackets.R
+++ R/brackets.R
@@ -1,23 +1,40 @@
+eat_brackets <- function (x, i) {
+  check_string(x)
+  nx <- nchar(x)
+  x <- reverse_strings(x)
+  i <- nx - i + 1
+  sc <- substr(x, i, i)
+  i <- pass_brackets(x, i)
+  i[1] <- nx - i[1] + 1
+  names(i) <- reverse_strings(i)
+  i
+}
+
 pass_brackets <- function (x, i) {
   sc <- substr(x, i, i)
+  names(i) <- sc
   nx <- nchar(x)  
   repeat {
     ii <- as.integer(regexpr("[][{}()]", substr(x, i + 1, nx)))
     if(ii == -1) {
       stop ("unmatched brackets")
     }
-    i <- i + ii
+    names(i) <- paste0(names(i),substr(x, i + 1, ii - 1)) 
+    i[1] <- i[1] + ii[1]
     ec <- substr(x, i, i)
     if(grepl("[[{(]", ec)) {
-      i <- pass_brackets(x, i)
+      ii <- pass_brackets(x, i)
+      names(i) <- paste0(names(i), names(ii))
+      i[1] <- ii[1]
     } else if (ec == switch(sc, 
       "[" = "]",
       "{" = "}",
       "(" = ")",
       stop("error"))) {
+      names(i) <- paste0(names(i), ec)
       return (i)
     } else {
       stop("unmatched brackets")
     }
   }
-}
+}  
--- tests/testthat/test-brackets.R
+++ tests/testthat/test-brackets.R
@@ -1,13 +1,23 @@
 context("pass_brackets")
 
 test_that("pass_brackets works on paired brackets", {
-  expect_identical(pass_brackets("()", 1), 2)
-  expect_identical(pass_brackets("(())", 1), 4)
-  expect_identical(pass_brackets("((({})))",1), 8)
-  expect_identical(pass_brackets("()()", 1), 2)
-  expect_identical(pass_brackets("()()", 3), 4)
-  expect_identical(pass_brackets("([])()", 1), 4)
-  expect_identical(pass_brackets("())))))))", 1), 2)
+  expect_equivalent(pass_brackets("()", 1), 2)
+  expect_equivalent(pass_brackets("(())", 1), 4)
+  expect_equivalent(pass_brackets("((({})))",1), 8)
+  expect_equivalent(pass_brackets("()()", 1), 2)
+  expect_equivalent(pass_brackets("()()", 3), 4)
+  expect_equivalent(pass_brackets("([])()", 1), 4)
+  expect_equivalent(pass_brackets("())))))))", 1), 2)
+})
+
+test_that("pass_brackets named by brackets", {
+  expect_equivalent(names(pass_brackets("()", 1)), "()")
+  expect_equivalent(names(pass_brackets("(())", 1)), "(())")
+  expect_equivalent(names(pass_brackets("((({})))",1)), "((({})))")
+  expect_equivalent(names(pass_brackets("()()", 1)), "()")
+  expect_equivalent(names(pass_brackets("()()", 3)), "()")
+  expect_equivalent(names(pass_brackets("([ \n ])()", 1)), "([ \n ])")
+  expect_equivalent(names(pass_brackets("())))))))", 1)), "()")
 })
 
 test_that("pass_brackets fails on unpaired brackets", {
