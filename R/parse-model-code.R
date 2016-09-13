c2fdf02fa09d10ac149b507167646de26020d254
joethorley <joe@poissonconsulting.ca>
2016-09-13 10:21:14 -0700

    working on parse_model_code

---
+++ R/parse-model-code.R
@@ -0,0 +1,28 @@
+parse_blocks <- function(x, i) {
+  if (missing(i)) i <- 1
+  x <- check_string(x)
+  nx <- nchar(x)
+  sc <- substr(x, i, i)
+  blocks <- character(0)
+  nblocks <- 0
+  repeat{
+    ii <- regexpr("^\\s*[A-Za-z][\\w.]*\\s*(?=[{])", substr(x, i, nx), perl = TRUE)
+    if(ii == -1)
+      stop("invalid block name syntax")
+    block_name <- regmatches(substr(x, i, nx), ii)
+    i <- i + ii + attr(ii, "match.length") - 1
+    i <- pass_brackets(x, i)
+    i[1] <- i + 1 
+    nblocks <- nblocks + 1
+    blocks[nblocks] <- names(i)
+    names(blocks)[nblocks] <- gsub("\\s*", "", block_name)
+    if(grepl("^\\s*$", substr(x, i, nx)))
+      return (blocks)
+  }
+}
+
+parse_model_code <- function(model_code) {
+  check_string(model_code)
+  
+  
+}
